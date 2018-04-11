8eb06539b72a393a76db771f65ae1c2b8f4bb8ae
Budi Arsana <gusdecool@gmail.com>
2018-04-11 21:26:38 +0800

    update the order of script

--- server-command.sh
+++ server-command.sh
@@ -1,12 +1,12 @@
 lsb_release -a # server information
 
+# installation Apache & PHP
+sudo apt-get install apache2 libapache2-mod-php php-cli php-curl php-mysql php-mcrypt php-zip composer
+
 # User group
 sudo usermod -a -G www-data gusdecool # add existing group to existing user
 sudo chown -R root:www-data /var/www/ # set group www-data to folder www/
 sudo chmod -R 775 /var/www/ # chmod to allow group
-
-# installation Apache & PHP
-sudo apt-get install apache2 libapache2-mod-php php-cli php-curl php-mysql php-mcrypt php-zip composer
 
 # apache
 sudo a2ensite # enable site
