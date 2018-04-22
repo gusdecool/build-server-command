e8b633232d0c1169da65d4770a3cf9cb8c270d47
Budi Arsana <gusdecool@gmail.com>
2018-04-22 12:58:14 +0800

    add wkthmltopdf requirement

--- server-command.sh
+++ server-command.sh
@@ -3,6 +3,9 @@ lsb_release -a # server information
 # installation Apache & PHP
 sudo apt-get install zip apache2 libapache2-mod-php php-cli php-curl php-mysql php-mcrypt php-zip php-dom composer
 
+# wkhtmltopdf requirements on debian v9
+sudo apt-get install libxrender1 libfontconfig libxext6
+
 # User group
 sudo usermod -a -G www-data gusdecool # add existing group to existing user
 sudo chown -R root:www-data /var/www/ # set group www-data to folder www/
